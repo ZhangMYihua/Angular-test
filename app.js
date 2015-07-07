4ba9e5d90f8663fede1262e533be976612722270
zhangmyihua <michaelzhang2@msn.com>
2015-07-08 06:10:59 +0800

    added ngmessages module as a dependancy, basic angular form

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ["ngMessages"]);
 
-myApp.controller('mainController', function() {
+myApp.controller('mainController', function($scope) {
 
 });
+
+var searchPeople = function(firstName, lastName, height, age, occupation) {
+  return 'Jane Doe';
+}
