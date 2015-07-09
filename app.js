e70be24cdb343c9368a4adf4a5e92e9fe6176f5b
zhangmyihua <michaelzhang2@msn.com>
2015-07-10 00:54:44 +0800

    trying out filter and data binding on text input for currency

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var myApp = angular.module('myApp', ["ngMessages", "ngResource"]);
 
 
-myApp.controller('mainController', ['$scope', function($scope)] {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 // Order here matters, the argument in the function should line with the positon of $scope
 // The name of the arguments doesn't matter, it can be called anything
 
-  $scope.handle = '';
-});
+  $scope.amount = '';
+
+  $scope.cashify = function(){
+    return $filter('currency')($scope.amount)
+  };
+}]);
 
 
